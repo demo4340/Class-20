a3fa53bfde363b47e75fbaefe78524d469242b6e
demo4340 <70212084+demo4340@users.noreply.github.com>
2022-08-08 20:52:40 +0530

    Add files via upload

---
+++ pro-c20-ta-boilerplate-main/sketch.js
@@ -0,0 +1,34 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+
+function setup()
+{
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ball_options = {
+    restitution : 0.95,
+    frictionAir : 0.01
+  }
+
+  ball = Bodies.circle(100, 10, 20, ball_options);
+  World.add(world,ball);
+
+}
+
+function draw() 
+{
+  background(51);
+ Engine.update(engine);
+ ellipse(ball.position.x, ball.position.y, 20,20)
+
+}
+
